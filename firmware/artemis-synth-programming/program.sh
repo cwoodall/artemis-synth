373d125072c12c0c193a4a8f10e5bd3e26097f8e
Christopher Woodall <chris.j.woodall@gmail.com>
2013-07-24 13:51:08 -0400

    main

--- firmware/artemis-synth-programming/program.sh
+++
@@ -1,9 +0,0 @@
-while true; do
-		printf "Programming Fuses!\n"
-		make fuse
-
-		printf "Programming Flash!\n"
-		make flash
-		printf "\nDone Programming, Check for Success.\n\nHit ENTER to program another, hit ^C to exit.\n"
-		read
-done
